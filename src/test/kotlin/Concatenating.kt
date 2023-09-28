5799bfb899ba95ab7865bc0f4537da609f1ac6dc
Timo <developer.timo@gmail.com>
2023-09-28 16:51:44 +0900

    feat: Concatenating strings and variable declaration

--- src/test/kotlin/Concatenating.kt
+++ src/test/kotlin/Concatenating.kt
@@ -17,4 +17,20 @@ class Concatenating {
         assertThat(string1).isEqualTo("Hello,")
         assertThat(string2).isEqualTo("world")
     }
+
+    @Test
+    fun concatenatingVariables() {
+        //given
+        val name = "Timo"
+        var greeting = "Welcome to Kotlin, "
+
+        //when
+        greeting += name
+
+        //then
+        print(greeting)
+        assertThat(greeting).isNotEqualTo("Welcome to Kotlin, ")
+        assertThat(greeting).isEqualTo("Welcome to Kotlin, Timo")
+        assertThat(name).isEqualTo("Timo")
+    }
 }
